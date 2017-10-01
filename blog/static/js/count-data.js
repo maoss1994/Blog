bd27ad21ddb12c14b52a0928c7ec4a0c751588fa
Cluas <Cluas@live.cn>
2017-10-01 15:59:40 +0800

    更新静态文件

---
+++ blog/static/js/count-data.js
@@ -0,0 +1,5 @@
+var DISQUSWIDGETS;
+
+if (typeof DISQUSWIDGETS != 'undefined') {
+    DISQUSWIDGETS.displayCount({"text":{"and":"\u53ca","comments":{"zero":"0 Comments","multiple":"{num} Comments","one":"1 Comment"}},"counts":[{"id":"2016\/next-documentations-reload\/","comments":2},{"id":"2015\/upgrade-computer-components\/","comments":1},{"id":"2016\/publishing-github-pages-with-travis-ci\/","comments":11},{"id":"2015\/something-about-next\/","comments":14}]});
+}
